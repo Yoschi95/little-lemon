bc3d305e16bcecb99c150d6ea1c5079d6a4eee70
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-09 22:08:14 +0200

    add footer content

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,32 @@
 function Footer() {
     return (
         <footer>
-            Footer
+            <img src="./img/Logo.svg" alt="Logo of the Little Lemon restaurant"/>
+            <br></br>
+            
+            Navigation
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order online</li>
+                <li>Login</li>
+            </ul>
+
+            Contact
+            <ul>
+                <li>Adress: First Street 1</li>
+                <li>Phone number: 123456789</li>
+                <li>Email: info@littlelemon.com</li>
+            </ul>
+
+            Social Media
+            <ul>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>LinkedIn</li>
+            </ul>
         </footer>
     )
 }
