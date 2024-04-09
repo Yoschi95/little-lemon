801115dea1ef75f814f3bb38a0ddf548bb6bed77
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-09 22:08:06 +0200

    add nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,14 @@
 function Nav() {
     return (
         <nav>
-            Nav
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order online</li>
+                <li>Login</li>
+            </ul>
         </nav>
     )
 }
