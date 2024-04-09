5dc8459286a28d9b2587eb3cff62894242a02e74
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-09 21:36:53 +0200

    replace semantic tags by related components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import './App.css';
+import Header from './Header'
+import Nav from './Nav'
+import Main from './Main'
+import Footer from './Footer'
 
 function App() {
   return (
     <>
-      <header>
-        header
-      </header>
-
-      <nav>
-        nav
-      </nav>
-
-      <main>
-        main
-      </main>
-
-      <footer>
-        footer
-      </footer>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
     </>
   );
 }
