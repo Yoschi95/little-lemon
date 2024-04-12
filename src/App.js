a4e1a9f1a944e4d86dbd7a2e43e2c3ba663b9da4
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-12 19:35:10 +0200

    adapt routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/layout/Header'
 import Nav from './components/layout/Nav'
+import Main from './components/layout/Main'
 import Footer from './components/layout/Footer'
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-import Menu from './components/pages/Menu'
-import Reservations from './components/pages/Reservations'
-import Order from './components/pages/Order'
-import Login from './components/pages/Login'
 
 function App() {
   return (
     <>
-    <Router>
-      <Header/>
-      <Nav/>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Menu" element={<Menu />} />
-          <Route path="/Reservations" element={<Reservations />} />
-          <Route path="/Order" element={<Order />} />
-          <Route path="/Login" element={<Login />} />
-      </Routes>
-      <Footer/>
-    </Router>
+      <Router>
+          <Header/>
+          <Nav/>
+          <Main/>
+          <Footer/>
+      </Router>
     </>
   );
 }
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './Main.css';
 import Home from '../pages/Home'
 import About from '../pages/About'
@@ -9,18 +9,14 @@ import Login from '../pages/Login'
 
 function Main() {
     return (
-        <main className="main">
-            <Router>
-                <Routes>
-                    <Route path="/Home" element={<Home />} />
-                    <Route path="/About" element={<About />} />
-                    <Route path="/Menu" element={<Menu />} />
-                    <Route path="/Reservations" element={<Reservations />} />
-                    <Route path="/Order" element={<Order />} />
-                    <Route path="/Login" element={<Login />} />
-                </Routes>
-            </Router>
-        </main>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Menu" element={<Menu />} />
+            <Route path="/Reservations" element={<Reservations />} />
+            <Route path="/Order" element={<Order />} />
+            <Route path="/Login" element={<Login />} />
+        </Routes>
     )
 }
 
