636debfabf85ce0a5c2f84ab757bbe6a6a3ae59e
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-13 23:51:07 +0200

    Style the reservation page

--- src/components/pages/Reservations.js
+++ src/components/pages/Reservations.js
@@ -1,9 +1,12 @@
 import './Reservations.css';
+import ReservationForm from "../shared/ReservationForm";
 
 function Reservations() {
     return (
         <main className="reservations">
-            Reservations
+            <h1>Reservation</h1>
+            <h2>Please enter the required information:</h2>
+            <ReservationForm />
         </main>
     )
 }
