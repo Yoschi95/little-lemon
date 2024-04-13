5b175a50d11518be624111870b8d9041a2915cb0
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-14 00:00:12 +0200

    Add occasion label

--- src/components/shared/ReservationForm.js
+++ src/components/shared/ReservationForm.js
@@ -11,34 +11,58 @@ const ReservationForm = () => {
 
   const [guests, setGuests] = useState('');
 
+  const [occasion, setOccasion] = useState('');
 
 
   const handleSubmit = (event) => {
 
     event.preventDefault();
-    console.log(`Submitting reservation for ${name} on ${date} at ${time} for ${guests} guests.`);
+    console.log(`Submitting reservation for ${name} on ${date} at ${time} for ${guests} guests. Occasion: ${occasion}.`);
 
   }
 
   return (
 
-    <form onSubmit={handleSubmit}><label>
+    <form onSubmit={handleSubmit}>
 
+      <label>
         Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+      </label>
 
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required /></label><br/><label>
+      <br/>
 
+      <label>
         Date:
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+      </label>
 
-        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required /></label><br/><label>
+      <br/>
 
+      <label>
         Time:
+        <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required />
+      </label>
 
-        <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required /></label><br/><label>
+      <br/>
 
+      <label>
         Number of Guests:
+        <input type="number" value={guests} onChange={(e) => setGuests(e.target.value)} min="1" required />
+      </label>
 
-        <input type="number" value={guests} onChange={(e) => setGuests(e.target.value)} min="1" required /></label><br/><button type="submit">Book Table</button></form>
+      <br/>
+
+      <label>
+        Occasion:
+        <input type="text" value={occasion} onChange={(e) => setOccasion(e.target.value)} required />
+      </label>
+
+      <br/>
+
+      <button type="submit">Book Table</button>
+
+    </form>
 
   );
 
