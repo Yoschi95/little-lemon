1cc40044f789f6479aa131c4240edeec8ce67534
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-13 23:13:30 +0200

    Create home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,20 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
+import ReserveButton from '../shared/ReserveButton';
 
 function Home() {
     return (
         <main className="home">
-            Home
+            <div className='leftSide'>
+                <h1>Litte Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <ReserveButton onClick={() => {return(<Link to="/Reservations"></Link>)}}> Reserve table </ReserveButton>
+            </div>
+
+            <div className='rightSide'>
+                <img src="./img/restaurant.jpg" alt="Logo of the Little Lemon restaurant" className='logo'/>
+            </div>
         </main>
     )
 }
