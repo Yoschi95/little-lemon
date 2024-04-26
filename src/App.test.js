5346986d212f255f4342e9f994cc74ef89de0255
Joscha Hartmann <joscha.hartmann@web.de>
2024-04-26 22:57:08 +0200

    Adjust unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,15 @@
 import { render, screen, fireEvent, within } from "@testing-library/react";
 import App from './App';
 
-test('Renders the App component, navigate to reservationForm and check time array after initializeTimes', () => {
+
+test('Renders the App component, navigate to reservationForm and check if Date text is rendered', () => {
         
-    let arrAvailableTimes = ['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00'];
     render(<App />);
 
     const reservationButton = screen.getByTestId("reservationButton")
     fireEvent.click(reservationButton); // Navigate to reservation page
-
-    const selectTextElement = screen.getByTestId("selectTime")
     
-    arrAvailableTimes.forEach((arrAvailableTimes) => {
+    const labelElement = screen.getByText("Date");
+    expect(labelElement).toBeInTheDocument();
 
-        expect(within(selectTextElement).getByText(arrAvailableTimes)).toBeInTheDocument();
-
-    });
-
-})
-
-
-test('Renders the App component, navigate to reservationForm and check time array after updateTimes', () => {
-        
-    let arrAvailableTimes = ['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00'];
-    render(<App />);
-
-    const selectTextElement = screen.getByTestId("selectTime");
-    fireEvent.change(selectTextElement, { target: { value: "17:00" } }); // select a element, so that the dispatch function gets called
-    
-    arrAvailableTimes.forEach((arrAvailableTimes) => {
-
-        expect(within(selectTextElement).getByText(arrAvailableTimes)).toBeInTheDocument();
-
-    });
-
-})
+});
